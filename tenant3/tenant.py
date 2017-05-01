ca535b7d4251617f85e62950a9b2c8a406d2f89f
khoale88 <lenguyenkhoa1988@gmail.com>
2017-04-30 21:17:19 -0700

    initial commit

---
+++ tenant3/tenant.py
@@ -0,0 +1,28 @@
+from flask import Flask, json, Response, request,render_template, url_for, redirect
+import save_and_parse as sap
+
+app = Flask(__name__)
+
+app.config["PARSER"] = "./parser/parser.jar"
+app.config["SAVE_FOLDER"] = "./textcodes/"
+app.config["OUTPUT_FOLDER"] = "./static/"
+
+
+@app.route('/textUpload', methods=['POST'])
+def upload_text():
+    body = request.json["textCode"]
+    sap.process(app.config["PARSER"],
+                app.config["SAVE_FOLDER"],
+                body,app.config["OUTPUT_FOLDER"])
+
+    res_json = {}
+    res_json["link"] = url_for("static", filename="output.png")
+
+    return Response(response=json.dumps(res_json), status=200)
+
+@app.route("/",methods=['GET'])
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
---
+++ tenant4/save_and_parse.py
@@ -0,0 +1,45 @@
+import subprocess
+import os
+
+def process(parser_path, save_path, data, output_path):
+    """save data into a file in save_path directory as sequence.java
+    and parse the file with output saved in output_path"""
+
+    #get the absolute path of save_path and output_path
+    save_path = os.path.abspath(save_path)
+    output_path = os.path.abspath(output_path)
+    file_name = "umlgraph"
+    file_ext = ".java"
+
+    #save data to folder and get the whole path
+    seq_path = save_to_folder(save_path, file_name + file_ext, data)
+    #parse the file
+    parse_uml(parser_path, seq_path)
+
+    #move file from save_path directory to output_path folder and rename to output.png
+    ori = os.path.join(save_path, file_name + ".png")
+    des = os.path.join(output_path, "output.png")
+    os.rename(ori, des)
+
+    return des
+
+
+def parse_uml(parser_path, input_path):
+    """parse_uml needs 2 inputs: a path to parser file,
+    and an input_path to folder where sequence file resides.
+    the result is store in the same input_path directory"""
+
+    #exclude the ".java" before parse
+    input_path = input_path[:-5]
+    subprocess.call([parser_path, input_path, "png"])
+
+def save_to_folder(save_path, save_name, data):
+    """save data into a file in save_path directory with name given by save_name
+    return the path to the file"""
+
+    path = os.path.join(save_path, save_name)
+    myfile = open(path, "w")
+    myfile.write(data)
+    myfile.close()
+    return path
+
